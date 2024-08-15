d2a70da3931ece1b067075761248cd33ecbca1f7
Do Yeong Kim <do@desktop-d8qctmg.lan>
2024-08-15 14:33:34 +1000

    gh pages configuration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://doyeongk.github.io/mvsoce-docs',
+  url: 'https://doyeongk.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/mvsoce-docs/',
