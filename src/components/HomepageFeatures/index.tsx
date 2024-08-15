48ff3c68891fc4a798b1ea34b16163820f737d4b
Do Yeong Kim <do@desktop-d8qctmg.lan>
2024-08-15 19:28:19 +1000

    uplift homepage

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -10,32 +10,32 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
+    title: 'Tournament Stats',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        The MultiVersus OCE website displays tournament statistics from 
+        start.gg. The data is stored in a Postgres database. 
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'MultiVersus API',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        The MVS OCE Dev Team have created wrappers for the MultiVersus API in 
+        C++ and Python.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Discord Bot',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        The MultiVersus OCE Discord Bot is used to facilitate tournaments through
+        integration with the start.gg API.
       </>
     ),
   },
